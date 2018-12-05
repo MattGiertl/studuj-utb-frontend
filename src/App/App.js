88ccb162574257fbf651d70222855d8622ed4dc7
Matúš Giertl <giertl@giertlmac2.domain.local>
2018-12-05 12:31:41 +0100

    basic structure added

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from '../atoms/Button/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button>Tlacitko</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
